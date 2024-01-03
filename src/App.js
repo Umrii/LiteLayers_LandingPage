c6e865f4a44e348907632bc07e8c1415f56f565d
Umrii <aatiq145@gmail.com>
2024-01-04 01:20:47 +0500

    changes update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
+import Changebg from "./Components/Changebg";
+import Categories from "./Components/Categories";
+import Tell from "./Components/Tell_us";
 function App() {
   return (
     <div>
       <Navbar />
-      <About />
+      <Changebg />
+      <Categories />
+      {/* <Tell /> */}
+      {/* <About /> */}
+
       <Footer />
     </div>
   );
