027d2d3193a083e6c64e7ccb10b26ccc3fb81a79
Umrii <aatiq145@gmail.com>
2024-01-06 20:17:11 +0500

    Improvements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './Navbar.css'; // Importing the CSS file
-import image from '../Assets/logo.png'
+import React from "react";
+import "./Navbar.css"; // Importing the CSS file
+import image from "../Assets/logo.png";
+import logos from "../Assets/LiteLayers.png";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <img src={image} alt="Lite Layer Logo" className="logo" />
+        <img src={logos} alt="Lite Layer Logo" className="logo" />
         <div class="logo-divider"></div>
         <div class="text-top">LITE</div>
-  <div class="text-bottom">LAYER</div>
+        <div class="text-bottom">LAYER</div>
       </div>
       <div className="buttons-container">
-        <button className="browse-now"><strong>Browse Now</strong></button>
+        <button className="browse-now">
+          <strong>Browse Now</strong>
+        </button>
         <button className="button grab-the-app">Grab the app</button>
       </div>
     </nav>
