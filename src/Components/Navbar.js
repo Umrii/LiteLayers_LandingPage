8d9716ddb1c927750c305c06e1ab825c43534281
Umrii <aatiq145@gmail.com>
2024-01-25 15:36:02 +0500

    Improvements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css"; // Importing the CSS file
 // import image from "../Assets/logo.png";
 import logos from "../Assets/LiteLayers.png";
-import btnQR from "../Assets/button-qr.png";
+// import btnQR from "../Assets/button-qr.png";
 
 const Navbar = () => {
   return (
@@ -18,12 +18,12 @@ const Navbar = () => {
             </div>
             <div className="buttons-container">
               <button className="browse-now">Browse Now</button>
-              <a href="#download">
-                <button className="button grab-the-app">
-                  <img className="btnQR" src={btnQR} alt="qrcode" />
-                  Download App
-                </button>
-              </a>
+              {/* <a href="#download"> */}
+              <button className="button grab-the-app">
+                {/* <img className="btnQR" src={btnQR} alt="qrcode" /> */}
+                Grab the app
+              </button>
+              {/* </a> */}
             </div>
           </nav>
         </div>
